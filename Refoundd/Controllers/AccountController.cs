efaabb1553c04578e6f4d2eb2ec7b12965b2dcd8
yasmeen <y.abuarrah1@gmail.com>
2026-01-16 19:11:38 +0200

    Add login page UI, AccountController, and view model
    Implemented AccountController with login/register/logout actions and TempData messages. Added LoginViewModel with validation (note: currently inherits Controller, should be fixed). Created styled Login.cshtml view and login.css for responsive, modern UI. No database integration yet.

---
+++ Refoundd/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Refoundd.Models;  
+
+namespace Refoundd.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["SuccessMessage"] = "Login functionality will be connected to database";
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+
+        // GET: Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["SuccessMessage"] = "Registration successful! Please login.";
+                return RedirectToAction("Login");
+            }
+
+            return View(model);
+        }
+
+        // GET: Account/Logout
+        public IActionResult Logout()
+        {
+            TempData["SuccessMessage"] = "You have been logged out successfully";
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Refoundd/Models/LoginViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+using Microsoft.EntityFrameworkCore;
+using Refoundd.Models;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Refoundd.Models
+{
+    public class LoginViewModel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Required(ErrorMessage = "University email is required")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        [Display(Name = "University Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
