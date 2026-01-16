b14758f2c4a25d8fdbda4877fd6c0ee0260b25fb
assma122 <asmayahya330@gmail.com>
2026-01-16 12:19:31 +0200

    add controllers user and  item and home

---
+++ Refoundd/Controllers/ItemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Refoundd.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
